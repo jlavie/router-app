69e804917c9c050876884280f397fa4360d28c1c
petitlardon <lavie.jul@gmail.com>
2025-09-10 01:09:38 +0200

    add next prev navigation on products. configuration with array index

--- src/app/products/product-item/product-item.component.ts
+++ src/app/products/product-item/product-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, computed, effect, inject, input } from '@angular/core';
 import { ProductsService } from '../products.service';
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { Router, RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-product-item',
@@ -9,11 +9,31 @@ import { RouterLink, RouterOutlet } from "@angular/router";
   styleUrl: './product-item.component.scss'
 })
 export class ProductItemComponent {
-  productId = input.required<number>();
+  productId = input.required();
   private productsService = inject(ProductsService);
+  private router = inject(Router);
 
   product = computed(() => this.productsService.products.find(p => p.id == this.productId()))
+  products = this.productsService.products;
 
+  currentIndex = computed(() => {
+    const id = this.productId();
+    // Trouve l'index dans products
+    return this.products.findIndex(p => p.id == id);
+  });
+
+  previousIndex = computed(() => this.currentIndex() > 0 ? this.currentIndex() - 1 : -1);
+  nextIndex = computed(() => this.currentIndex() < this.products.length - 1 ? this.currentIndex() + 1 : -1);
+
+  previousProductId = computed(() => this.previousIndex() >= 0 ? this.products[this.previousIndex()].id : null);
+  nextProductId = computed(() => this.nextIndex() >= 0 ? this.products[this.nextIndex()].id : null);
+
+  navigateToProduct(id: any) {
+    if(id !== null) {
+      // Navigation sans rechargement, Angular gère le routing SPA
+      this.router.navigate(['/products', id]);
+    }
+  }
   // constructor() {
   //   effect(() => {
   //     console.log('productId:', this.productId());
